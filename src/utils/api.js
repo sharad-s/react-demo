353a66880c1a97713119a1f44404ee25926c9070
sharad-s <sharadshekar@gmail.com>
2019-10-28 02:08:32 -0500

    Quickfix: Youtube API Key in Prod

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,13 @@
 import axios from "axios"
-import { YOUTUBE_API_KEY } from "./keys"
+// import { YOUTUBE_API_KEY } from "./keys"
+
+
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
     params: {
         part: 'snippet',
         maxResults: 5,
-        key: YOUTUBE_API_KEY
+        key: "AIzaSyAOkk4NVDrbS_6WyXXrPv8vW3buyY2flJY"
     }
 })
